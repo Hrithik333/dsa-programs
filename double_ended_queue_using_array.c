312df8c8aa6daf295ea670721150767af06aba14
Hrithik333 <67138916+Hrithik333@users.noreply.github.com>
2020-10-08 00:35:46 +0530

    Create double_ended_queue_using_array.c

---
+++ double_ended_queue_using_array.c
@@ -0,0 +1,69 @@
+//hrithik tiwari
+#include<stdio.h>
+#define MAX 20
+
+typedef struct{
+  int data[MAX];
+  int f;//front
+  int R;//rear
+  }Dqueue;
+  
+  int insert(Dqueue *q,int v,int side){
+  if(q->f==0&&q->R==MAX-1){//full queue
+  printf("Q id full\n");
+  return 1;
+  }
+  if(q->R== -1){//empty queue
+  q->f=q->R=0;
+  q->data[q->R]=v;
+  return 0;
+  }
+  if(side==1){
+  if(q->R!=MAX-1){//0(1)
+  q->R++;
+  q->data[q->R]=v;
+  }else{//0(n)
+  for(int i=q->f;i<=q->R;i++){
+  q->data[i-1]=q->data[i];
+  }
+  q->data[q->f]=v;
+  q->R++;
+  }
+  }
+  return 0;
+  }
+  int del(Dqueue *q;int *m;int side){
+  if(q->f==-1){//Empty Q
+  printf("Q is Empty\n");
+  return 1;
+  }
+  if(q->f==q->R){//Single element present 
+  *m=q->data[q->f];
+  q->f=q->R=-1;
+  }else if(side==0){//deleting front front
+  *m=q->data[q->R];
+  q->R--;
+  }
+  return 0;
+  }
+  void display(Dqueue q){
+  int i;
+  if(q.f==-1)
+  printf("Queue is empty \n");
+  else{
+  for(i=q.f;i<=q.R;i++)
+  printf("%d",q.data[i]);
+  printf("\n");
+  }
+  }
+  int main()
+  Dqueue q1;
+  q1.f=q1.R=-1;
+  int p=insert(&q1,10,0);
+  p=insert(&q1,20,1);
+  display(q1);
+  int m;
+  int q=del(&q1,&m,0);
+  display(q1);
+  return 0;
+  }
